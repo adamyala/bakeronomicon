d69a2ff1c0fc18169f784420b85953a1b99804a8
Adam Yala <adamkyala@gmail.com>
2021-09-28 17:32:50 -0600

    add whitenoise

--- bakeronomicon/settings.py
+++ bakeronomicon/settings.py
@@ -140,19 +140,8 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn=os.environ['SENTRY_DSN'],
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+# whitenoise middleware serves static files in heroku
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# when collectstatic is run on a server, it will put all collected files
+# in a dir called staticfiles
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
