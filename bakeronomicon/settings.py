838f43e5d96f3591a84d3f9f4ffd06f7e207e7ab
Adam Yala <adamkyala@gmail.com>
2021-09-28 18:33:48 -0600

    add whitenoise middleware

--- bakeronomicon/settings.py
+++ bakeronomicon/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
